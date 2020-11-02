039a1c094e7cd99cd83f16e1e4d9a2e93ddbf8a8
Lincer6 <Dober-01@yandex.ua>
2020-11-02 15:50:43 +0200

    Main Layout finished

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const animation = () => {
     let elementsLeft
     let elementsRight
+    let elementsUnderline
     let windowHeight
 
     const init = () => {
         elementsLeft = document.querySelectorAll('.hiddenLeft')
         elementsRight = document.querySelectorAll('.hiddenRight')
+        elementsUnderline = document.querySelectorAll('.hiddenUnderline')
         windowHeight = window.innerHeight
     }
 
@@ -26,6 +28,19 @@ const animation = () => {
                     element.classList.remove('hiddenRight')
                     element.style.animation = 'popUp'
                     element.style.animationDuration = '1s'
+
+                }, 500)
+            }
+        })
+
+        elementsUnderline.forEach(element => {
+            const positionFromTop = element.getBoundingClientRect().top;
+            if (positionFromTop - windowHeight <= 0) {
+                setTimeout(() => {
+                    element.classList.remove('hiddenUnderline')
+                    element.style.opacity = '1'
+                    element.style.width = '100%'
+                    element.style.transition = 'width 1s'
 
                 }, 500)
             }
